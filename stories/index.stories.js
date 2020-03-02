dd7440ebcf273954967a561fe3170396965d3130
Mariz Melo <mm@xchema.com>
2020-03-01 17:12:22 -0800

    Update for buttons, New forms package with all form elements styles

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,6 +1,8 @@
 import '../packages/snap-css/snap-css.css';
+import '../packages/snap-css-colors/colors.css';
 import '../packages/snap-css-typography/typography.css';
 import '../packages/snap-css-layout/layout.css';
+import '../packages/snap-css-forms/forms.css';
 import '../packages/snap-css-buttons/buttons.css';
 
 export default {
@@ -20,6 +22,34 @@ export const Typography = () => `
   <h6>Heading 6</h6>
 `;
 
-export const Forms = () => `<button>Hello Button</button>`;
+export const Forms = () => `
+  <input type="text" placeholder="Type text">
+  <br>
+  <input type="email" placeholder="type an e-mail address">
+  <br>
+  <input type="color" placeholder="Color picker">
+  <br>
+  <input type="file" placeholder="Color picker">
+  <br>
+  <input type="date" placeholder="Color picker">
+  <br>
+  <input type="month" placeholder="Color picker">
+  <br>
+  <input type="time" placeholder="Color picker">
+  <br>
+  <input type="number">
+  <br>
+  <input type="password" placeholder="Enter a password">
+  <br>
+  <input type="checkbox">
+  <br>
+  <input type="search" placeholder="Enter a search">
+  <br>
+  <input type="range">
+  <br>
+  <input type="reset">
+  <br>
+  <input type="url">
+`;
 
 export const Layout = () => `layout`;
